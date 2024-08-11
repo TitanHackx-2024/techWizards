f286227036a511246ce5c359a56cdb70e392abd8
guptabhishek4 <ag444982@gmail.com>
2024-08-11 13:14:13 +0530

    added code for top-sites and domain

---
+++ frontend/comp-analyzer/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { DomainCardComponent } from './domain-card/domain-card.component';
+import { AppComponent } from './app.component';
+import { TopSitesComponent } from '../top-sites/top-sites.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [
+    DomainCardComponent,
+    TopSitesComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
